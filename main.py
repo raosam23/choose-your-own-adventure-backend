bee4cccc0f9631659d618cf25da80a4fe1c0756a
Samarth Rao <raosamarth1234@gmail.com>
2026-02-18 08:46:05 +0530

    setup env variables, setup database model and created schemas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from core.config import settings
 
 app = FastAPI(
     title='Choose your own adventure Game API',
@@ -11,7 +12,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ['*'],
+    allow_origins = settings.ALLOWED_ORIGINS,
     allow_credentials = True,
     allow_methods = ['*'],
     allow_headers = ['*'],
